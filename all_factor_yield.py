337b5c9c11668cb0393b5cce962dd4df1eb01186
pywonne <ypan72@wisc.edu>
2021-06-28 15:46:15 +0800

    all added

--- all_factor_yield.py
+++ all_factor_yield.py
@@ -2,7 +2,7 @@
 @author：Yiwen Pan
 计算daily因子收益率向量f和特质收益率向量u
 
-portfolio：中证500
+portfolio：全仓
 '''
 from sys import float_repr_style
 from higgsboom.MarketData.CSecurityMarketDataUtils import *
@@ -16,51 +16,50 @@ import pickle
 import matplotlib.pyplot as plt
 import os
 
-# plt.rcParams["font.family"]="STSong"
-# plt.rcParams['axes.unicode_minus']=False
-LNCAP_file = open('C:/Users/panyi/Documents/BarraFactorsLibrary/Barra/CNE5_LNCAP.pkl', 'rb') 
-LNCAP_data = pd.DataFrame(pickle.load(LNCAP_file))
-Beta_file = open('C:/Users/panyi/Documents/BarraFactorsLibrary/Barra/CNE5_Beta.pkl', 'rb') 
-Beta_data = pd.DataFrame(pickle.load(Beta_file))
-BP_file = open('C:/Users/panyi/Documents/BarraFactorsLibrary/Barra/CNE5_BP.pkl', 'rb')
-BP_data = pd.DataFrame(pickle.load(BP_file))
-Earning_file = open('C:/Users/panyi/Documents/BarraFactorsLibrary/Barra/CNE5_Earning.pkl', 'rb')
-Earning_data = pd.DataFrame(pickle.load(Earning_file))
-Growth_file = open('C:/Users/panyi/Documents/BarraFactorsLibrary/Barra/CNE5_Growth.pkl', 'rb')
-Growth_data = pd.DataFrame(pickle.load(Growth_file))
-Leverage_file = open('C:/Users/panyi/Documents/BarraFactorsLibrary/Barra/CNE5_Leverage.pkl', 'rb')
-Leverage_data = pd.DataFrame(pickle.load(Leverage_file))
-Liquidity_file = open('C:/Users/panyi/Documents/BarraFactorsLibrary/Barra/CNE5_Liquidity_sum.pkl', 'rb')
-Liquidity_data = pd.DataFrame(pickle.load(Liquidity_file))
-Momentum_file = open('C:/Users/panyi/Documents/BarraFactorsLibrary/Barra/CNE5_RSTR.pkl', 'rb')
-Momentum_data = pd.DataFrame(pickle.load(Momentum_file)) 
-NLSize_file = open('C:/Users/panyi/Documents/BarraFactorsLibrary/Barra/CNE5_NLSIZE.pkl', 'rb')
-NLSize_data = pd.DataFrame(pickle.load(NLSize_file))
-Volatility_file = open('C:/Users/panyi/Documents/BarraFactorsLibrary/Barra/CNE5_Volatility.pkl', 'rb')
-Volatility_data = pd.DataFrame(pickle.load(Volatility_file))
 
-def get_daily_return(t, Index500_Factor):
+LNCAP_file = open('C:/Users/panyi/Documents/BarraFactorsLibrary/Barra/CNE5_LNCAP.pkl', 'rb') 
+LNCAP_data = pd.DataFrame(pickle.load(LNCAP_file)).T # index是股票，columns是日期
+Beta_file = open('C:/Users/panyi/Documents/BarraFactorsLibrary/Barra/CNE5_Beta.pkl', 'rb') 
+Beta_data = pd.DataFrame(pickle.load(Beta_file)).T
+BP_file = open('C:/Users/panyi/Documents/BarraFactorsLibrary/Barra/CNE5_BP.pkl', 'rb')
+BP_data = pd.DataFrame(pickle.load(BP_file)).T
+Earning_file = open('C:/Users/panyi/Documents/BarraFactorsLibrary/Barra/CNE5_Earning.pkl', 'rb')
+Earning_data = pd.DataFrame(pickle.load(Earning_file)).T
+Growth_file = open('C:/Users/panyi/Documents/BarraFactorsLibrary/Barra/CNE5_Growth.pkl', 'rb')
+Growth_data = pd.DataFrame(pickle.load(Growth_file)).T
+Leverage_file = open('C:/Users/panyi/Documents/BarraFactorsLibrary/Barra/CNE5_Leverage.pkl', 'rb')
+Leverage_data = pd.DataFrame(pickle.load(Leverage_file)).T
+Liquidity_file = open('C:/Users/panyi/Documents/BarraFactorsLibrary/Barra/CNE5_Liquidity_sum.pkl', 'rb')
+Liquidity_data = pd.DataFrame(pickle.load(Liquidity_file)).T
+Momentum_file = open('C:/Users/panyi/Documents/BarraFactorsLibrary/Barra/CNE5_RSTR.pkl', 'rb')
+Momentum_data = pd.DataFrame(pickle.load(Momentum_file)).T
+NLSize_file = open('C:/Users/panyi/Documents/BarraFactorsLibrary/Barra/CNE5_NLSIZE.pkl', 'rb')
+NLSize_data = pd.DataFrame(pickle.load(NLSize_file)).T
+Volatility_file = open('C:/Users/panyi/Documents/BarraFactorsLibrary/Barra/CNE5_Volatility.pkl', 'rb')
+Volatility_data = pd.DataFrame(pickle.load(Volatility_file)).T
+
+def get_daily_return(t, Style_Factor):
     '''
     计算股票每日收益率, （当日收盘价/昨日收盘价）-1
     t: 日期
     '''
-    RetFrame  = pd.DataFrame(index = Index500_Factor.index, columns =['r'])
-    for stock in Index500_Factor.index:
+    RetFrame  = pd.DataFrame(index = Style_Factor.index, columns =['r'])
+    for stock in Style_Factor.index:
         stock_daily_data = secUtils.StockDailyDataFrame(stock)
         stock_daily_data.index = stock_daily_data['TRADING_DATE']
         if t not in stock_daily_data.index:
-            Index500_Factor.drop(index=[stock], inplace=True)
+            Style_Factor.drop(index=[stock], inplace=True)
             RetFrame.drop(index=[stock], inplace=True)
             continue
         if(stock_daily_data.at[t, 'TRADE_STATUS'] != '交易'):
-            Index500_Factor.drop(index=[stock], inplace=True)
+            Style_Factor.drop(index=[stock], inplace=True)
             RetFrame.drop(index=[stock], inplace=True)
             continue        
         today_ret = stock_daily_data.at[t, 'CLOSE']
         yesterday_ret = stock_daily_data.shift(1).at[t, 'CLOSE']
         daily_ret = (today_ret / yesterday_ret) - 1
         RetFrame.at[stock, 'r'] = daily_ret
-    return RetFrame, Index500_Factor
+    return RetFrame, Style_Factor
 
 def neu_with_intercept(array_x, array_y):
     '''
@@ -90,7 +89,7 @@ def CAP_standard(array, floatcap):
     intersectID.sort()
     x = x[intersectID]
     y = y[intersectID]
-    temp = x - (x * y).sum() 
+    temp = x - (x * y).sum()
     std = x.std()
     return temp / std if std !=0 else temp
 
@@ -107,59 +106,59 @@ def Winsorize(x, limit = 3):
     win = x.apply(func)
     return win
 
-def get_X(t, Index500_StyleFactor):
+def get_X(t, StyleFactor):
     '''
     构建每日因子暴露矩阵X
     t: 日期
     ''' 
     # 去极值
-    Index500_StyleFactor = Winsorize(Index500_StyleFactor, limit=5)
+    StyleFactor = Winsorize(StyleFactor, limit=5)
     # 正交去除共线性
     # 风格因子标准化（流通市值）
-    CAP_data = pd.Series(np.exp(Index500_StyleFactor['LNCAP']))
+    CAP_data = pd.Series(np.exp(StyleFactor['LNCAP']))
     CAP_weights = pd.DataFrame(CAP_data / CAP_data.sum()) # 流通市值加权
     CAP_weights.columns =['weight']
-    Index500_StyleFactor['LNCAP'] = CAP_standard(Index500_StyleFactor['LNCAP'], CAP_weights['weight'])
-    Index500_StyleFactor['Beta'] = CAP_standard(Index500_StyleFactor['Beta'], CAP_weights['weight'])
-    Index500_StyleFactor['BP'] = CAP_standard(Index500_StyleFactor['BP'], CAP_weights['weight'])
-    Index500_StyleFactor['Earning'] = CAP_standard(Index500_StyleFactor['Earning'], CAP_weights['weight'])
-    Index500_StyleFactor['Growth'] = CAP_standard(Index500_StyleFactor['Growth'], CAP_weights['weight'])
-    Index500_StyleFactor['Leverage'] = CAP_standard(Index500_StyleFactor['Leverage'], CAP_weights['weight'])
-    Index500_StyleFactor['Liquidity'] = neu_with_intercept(Index500_StyleFactor['LNCAP'], Index500_StyleFactor['Liquidity'])
-    Index500_StyleFactor['Liquidity'] = CAP_standard(Index500_StyleFactor['Liquidity'], CAP_weights['weight'])
-    Index500_StyleFactor['Momentum'] = CAP_standard(Index500_StyleFactor['Momentum'], CAP_weights['weight'])
-    Index500_StyleFactor['NLSize'] = CAP_standard(Index500_StyleFactor['NLSize'], CAP_weights['weight'])
-    Index500_StyleFactor['Volatility'] = neu_with_intercept(Index500_StyleFactor['Beta'], Index500_StyleFactor['Volatility'])
-    Index500_StyleFactor['Volatility'] = neu_with_intercept(Index500_StyleFactor['LNCAP'], Index500_StyleFactor['Volatility'])
-    Index500_StyleFactor['Volatility'] = CAP_standard(Index500_StyleFactor['Volatility'], CAP_weights['weight'])
+    StyleFactor['LNCAP'] = CAP_standard(StyleFactor['LNCAP'], CAP_weights['weight'])
+    StyleFactor['Beta'] = CAP_standard(StyleFactor['Beta'], CAP_weights['weight'])
+    StyleFactor['BP'] = CAP_standard(StyleFactor['BP'], CAP_weights['weight'])
+    StyleFactor['Earning'] = CAP_standard(StyleFactor['Earning'], CAP_weights['weight'])
+    StyleFactor['Growth'] = CAP_standard(StyleFactor['Growth'], CAP_weights['weight'])
+    StyleFactor['Leverage'] = CAP_standard(StyleFactor['Leverage'], CAP_weights['weight'])
+    StyleFactor['Liquidity'] = neu_with_intercept(StyleFactor['LNCAP'], StyleFactor['Liquidity'])
+    StyleFactor['Liquidity'] = CAP_standard(StyleFactor['Liquidity'], CAP_weights['weight'])
+    StyleFactor['Momentum'] = CAP_standard(StyleFactor['Momentum'], CAP_weights['weight'])
+    StyleFactor['NLSize'] = CAP_standard(StyleFactor['NLSize'], CAP_weights['weight'])
+    StyleFactor['Volatility'] = neu_with_intercept(StyleFactor['Beta'], StyleFactor['Volatility'])
+    StyleFactor['Volatility'] = neu_with_intercept(StyleFactor['LNCAP'], StyleFactor['Volatility'])
+    StyleFactor['Volatility'] = CAP_standard(StyleFactor['Volatility'], CAP_weights['weight'])
     # 构建因子暴露度矩阵 X（1个国家因子+28个行业因子+10个风格因子）
     # 国家因子暴露
-    country_data = pd.Series(np.ones(len(Index500_StyleFactor)), index = Index500_StyleFactor.index, name = 'country').to_frame()
+    country_data = pd.Series(np.ones(len(StyleFactor)), index = StyleFactor.index, name = 'country').to_frame()
     # 行业因子暴露
     IndusCons = secUtils.DailyIndustryConstituents('SW', 'L1', t)   # 申万 L1, 28个行业分类
     IndusList = []
-    for i in Index500_StyleFactor.index:
+    for i in StyleFactor.index:
         for key in IndusCons:
             if i in IndusCons.get(key):
                 IndusList.append(key)
                 break
     IndusTmp = pd.DataFrame({'行业': IndusList})
     IndusFactor = pd.get_dummies(IndusTmp)
-    IndusFactor.index = Index500_StyleFactor.index
+    IndusFactor.index = StyleFactor.index
     # print(IndusFactor)
-    X = pd.concat([country_data, IndusFactor, Index500_StyleFactor], axis = 1)
+    X = pd.concat([country_data, IndusFactor, StyleFactor], axis = 1)
     # print(X) 
     # X.to_excel(os.path.join('C:/Users/panyi/Documents/BarraFactorsLibrary/X_daily',t+'.xlsx'))
-    return X, Index500_StyleFactor, IndusFactor
+    return X, StyleFactor, IndusFactor
 
 
-def get_factor_return(t, Index500_StyleFactor):
+def get_factor_return(t, StyleFactor):
     '''
     构建每日因子收益率f和特质股票收益率u
     t: 日期
     '''
-    r, adj_Style_Factor = get_daily_return(t, Index500_StyleFactor)
-    X, adj_Style_Factor, IndusFactor = get_X(t,adj_Style_Factor)
+    r, adj_Style_Factor = get_daily_return(t, StyleFactor)
+    X, adj_Style_Factor, IndusFactor= get_X(t,adj_Style_Factor)
     
     LNCAP_data = pd.Series(adj_Style_Factor['LNCAP']).to_frame()
     CAP_data = pd.DataFrame(np.exp(LNCAP_data))
@@ -203,39 +202,14 @@ def get_factor_return(t, Index500_StyleFactor):
     return f, u 
 
 def daily_calculate(t):
-    Index500_LNCAP = []
-    Index500_Beta = []
-    Index500_BP = []
-    Index500_Earning = []
-    Index500_Growth = []
-    Index500_Leverage = []
-    Index500_Liquidity = []
-    Index500_Momentum = []
-    Index500_NLSize = []
-    Index500_Volatility = []
-    IndexCons = secUtils.IndexConstituents('000905.SH', t) # 中证500股票池
-    # print(IndexCons)
-    for j in LNCAP_data.columns:
-        if j in IndexCons:
-            Index500_LNCAP.append(LNCAP_data.at[t,j])
-            Index500_Beta.append(Beta_data.at[t,j])
-            Index500_BP.append(BP_data.at[t,j])
-            Index500_Earning.append(Earning_data.at[t,j])
-            Index500_Growth.append(Growth_data.at[t,j])
-            Index500_Leverage.append(Leverage_data.at[t,j])
-            Index500_Liquidity.append(Liquidity_data.at[t,j])
-            Index500_Momentum.append(Momentum_data.at[t,j])
-            Index500_NLSize.append(NLSize_data.at[t,j])
-            Index500_Volatility.append(Volatility_data.at[t,j])
     # print(Index500_LNCAP) # 市值因子
-    Index500_dic = {'LNCAP': Index500_LNCAP, 'Beta': Index500_Beta, 'BP': Index500_BP, 'Earning': Index500_Earning, 'Growth': Index500_Growth,
-            'Leverage': Index500_Leverage, 'Liquidity': Index500_Liquidity,'Momentum': Index500_Momentum, 'NLSize': Index500_NLSize, 'Volatility': Index500_Volatility} 
-    Index500_StyleFactor = pd.DataFrame(Index500_dic, index=IndexCons) # daily style factor
-    Index500_StyleFactor.dropna(axis=0, how='any', inplace=True)
-    # print(Index500_StyleFactor)
-    # print(Index500_StyleFactor.isnull().any())
+    StyleFactor = pd.concat([LNCAP_data[t], Beta_data[t], BP_data[t], Earning_data[t], Growth_data[t], Leverage_data[t], Liquidity_data[t], Momentum_data[t],
+                    NLSize_data[t], Volatility_data[t]], axis=1) # daily style factor
+    StyleFactor.columns = namelist
+    StyleFactor.dropna(axis=0, how='any', inplace=True)
+    # print(StyleFactor)
     
-    f, u = get_factor_return(t, Index500_StyleFactor) 
+    f, u = get_factor_return(t, StyleFactor) 
 
     f.columns = [t]
     u.columns = [t]
@@ -265,20 +239,20 @@ def plot_return(cumf_ret, namelist):
     return
 
 
-PeriodList = TradingDays(startDate='2021-04-01', endDate='2021-04-09')
+
+namelist = ['LNCAP', 'Beta', 'BP', 'Earning', 'Growth', 'Leverage', 'Liquidity', 'Momentum', 'NLSize', 'Volatility']
+PeriodList = TradingDays(startDate='2018-01-01', endDate='2021-04-09')
 f_frame = pd.DataFrame() # index行业因子，columns日期
 u_frame = pd.DataFrame()
 pool = ThreadPool()
 f = pool.map(daily_calculate, PeriodList)
 f_frame = pd.concat(f, axis = 1)
 # u_frame = pd.concat(u, axis = 1)
-print(f_frame)
+# print(f_frame)
 # f_frame.to_excel('C:/Users/panyi/Documents/BarraFactorsLibrary/f_ret_final_2021.xlsx')
 # u_frame.to_excel('C:/Users/panyi/Documents/BarraFactorsLibrary/u_ret_final.xlsx')
 
 # 风格因子的pure factor return
-namelist = ['LNCAP', 'Beta', 'BP', 'Earning', 'Growth', 'Leverage', 'Liquidity', 'Momentum', 'NLSize', 'Volatility']
-
 f_stlye_frame = f_frame.loc[namelist, :]
 f_stlye_frame = f_stlye_frame.T
 cumf_style_frame = f_stlye_frame.cumsum()
